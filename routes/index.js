585a9a672231036d739c693c9ecbf040894e8122
thienan472 <an.ngo@chative.io>
2022-10-19 08:09:23 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/ver', (req, res) => res.json(200));
+
+module.exports = router;
